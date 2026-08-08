d04c5e86c1bde0305073c12ff1aba708899cb6fa
Dak <dakotawells96@gmail.com>
2026-08-08 03:10:17 -0400

    Fix issues with streams using the https protocol.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "ui/Application.h"
 #include <QApplication>
+#include <cstdlib>
 
 int main(int argc, char **argv) {
+    qputenv("QT_FFMPEG_PROTOCOL_WHITELIST", "file,crypto,data,https,tls,tcp,http,concat");
     QApplication qapp(argc, argv);
     Application app;
     return app.run(argc, argv);
